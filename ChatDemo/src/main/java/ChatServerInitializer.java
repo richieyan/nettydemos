e961d494ac282065a1cccf223fbe802f477cc30e
richie.yan <richie.yan@happyelements.com>
2016-08-15 16:28:56 +0800

    comment for understand handshake

--- ChatDemo/src/main/java/ChatServerInitializer.java
+++ ChatDemo/src/main/java/ChatServerInitializer.java
@@ -1,6 +1,4 @@
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelPipeline;
+import io.netty.channel.*;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
@@ -18,6 +16,16 @@ public class ChatServerInitializer extends ChannelInitializer<Channel> {
     public ChatServerInitializer(ChannelGroup group){
         this.group = group;
     }
+
+    /***
+     * WebSocketServerProtocolHandshakeHandler will be add into pipeline when WebSocketServerProtocolHandler#handlerAdded invoke.
+     * When the upgrade(handshake) is completed, the WebSocketServerHandshaker#handshake in WebSocketServerProtocolHandshakeHandler replaces
+     * the HttpRequestDecoder with a WebSocketFrameDecoder and the HttpResponseEncoder with a WebSocketFrameEncoder.
+     * To maximize performance it will then remove any ChannelHandlers that aren't required for WebSocket connections.
+     * These would include the HttpObjectAggregator and HttpRequestHandler.
+     * @param ch
+     * @throws Exception
+     */
     @Override
     protected void initChannel(Channel ch) throws Exception {
         ChannelPipeline pipeline = ch.pipeline();
@@ -27,7 +35,6 @@ public class ChatServerInitializer extends ChannelInitializer<Channel> {
         pipeline.addLast(new HttpRequestHandler("/ws"));
         pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
         pipeline.addLast(new TextWebSocketFrameHandler(group));
-
 
     }
 }
